c89aa7c25fb6c7f937dd3821b83e2abb3d18ad2f
talhariaz105 <muhammad.talha@fabtechsol.com>
2025-06-18 00:30:58 +0500

    well

--- src/config/otherRoutes.js
+++ src/config/otherRoutes.js
@@ -24,10 +24,7 @@ module.exports = (app) => {
   });
   app.use(globalErrorHandler);
 
-  const port = process.env.PORT;
-  server.listen(port, () => {
-    // initializeSocket(server)
-    
-    console.log(`Server running in ${process.env.NODE_ENV} mode on port ${port}`.yellow.bold);
-  });
+  const port = process.env.PORT || 3000;
+ app.listen(port,"0.0.0.0", () => console.log(`Example app listening on port ${port}!`));
+
 };
