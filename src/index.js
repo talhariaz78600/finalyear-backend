1ba6e2defe678099952f1b1a1b166a7f2ac4c2d7
talhariaz105 <muhammad.talha@fabtechsol.com>
2025-06-17 22:56:48 +0500

    well

--- src/index.js
+++ src/index.js
@@ -5,7 +5,8 @@ const passport = require('passport');
 const cookieParser = require('cookie-parser');
 const { connectDB } = require('./config/connectDb');
 const configMiddlewares = require('./config/configMiddlewares');
-
+const routes = require('./config/routes');
+// const connectRedis = require('./config/connectRedis');
 
 const app = express();
 app.use(cookieParser());
