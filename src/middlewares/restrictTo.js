48be9ed50257e438dbe36c43a8cecf44fcedf4cb
talhariaz105 <muhammad.talha@fabtechsol.com>
2025-06-18 01:08:15 +0500

    post

--- src/middlewares/restrictTo.js
+++ src/middlewares/restrictTo.js
@@ -8,16 +8,16 @@ module.exports =
       return next(new AppError('You do not have permission to perform this action', 403));
     }
 
-    if(req.user?.adminRole==="subAdmin"){
-      const currentTab = req.headers['x-tab'];
-      console.log("currentTab",currentTab)
-      if (!currentTab) {
-        return res.status(400).json({ message: 'Missing tab context' });
-      }
-      if (!req?.user?.templateId?.tabPermissions.includes(currentTab)) {
-        return res.status(403).json({ message: `User does not have access to tab: ${currentTab}` });
-      }
-    }
+    // if(req.user?.adminRole==="subAdmin"){
+    //   const currentTab = req.headers['x-tab'];
+    //   console.log("currentTab",currentTab)
+    //   if (!currentTab) {
+    //     return res.status(400).json({ message: 'Missing tab context' });
+    //   }
+    //   if (!req?.user?.templateId?.tabPermissions.includes(currentTab)) {
+    //     return res.status(403).json({ message: `User does not have access to tab: ${currentTab}` });
+    //   }
+    // }
 
     return next();
   };
